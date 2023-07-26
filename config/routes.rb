dddbcd97fe06e2a6fa827643458720f4762c6343
Fabiana M. M. Rodrigues <biamunhoz@gmail.com>
2023-07-26 15:03:19 -0300

    Esqueci a senha, ajustes gerais

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :salas, path: 'equipamentos'
   resources :agendas
   resources :events
+  resources :senha_resets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #inscricao e permissao
